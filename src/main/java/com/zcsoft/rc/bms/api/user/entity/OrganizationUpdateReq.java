e0fb974732fbfe2701c8dda99faed3dd3657bfca
joly001 <joly_huang@163.com>
2019-03-24 22:06:52 +0800

    init

---
+++ src/main/java/com/zcsoft/rc/bms/api/user/entity/OrganizationUpdateReq.java
@@ -0,0 +1,69 @@
+package com.zcsoft.rc.bms.api.user.entity;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class OrganizationUpdateReq {
+
+    /**
+     *
+     */
+    @NotEmpty
+    private String id;
+    /**
+     * 组织名称
+     */
+    @NotEmpty
+    private String orgName;
+    /**
+     * 组织描述
+     */
+    private String orgDesc;
+    /**
+     * 排序号
+     */
+    private Integer sequenceNumber;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public String getOrgDesc() {
+        return orgDesc;
+    }
+
+    public void setOrgDesc(String orgDesc) {
+        this.orgDesc = orgDesc;
+    }
+
+    public Integer getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    public void setSequenceNumber(Integer sequenceNumber) {
+        this.sequenceNumber = sequenceNumber;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("OrganizationUpdateReq{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", orgName='").append(orgName).append('\'');
+        sb.append(", orgDesc='").append(orgDesc).append('\'');
+        sb.append(", sequenceNumber=").append(sequenceNumber);
+        sb.append('}');
+        return sb.toString();
+    }
+}

1878fa4527e1955c910641f0b5a3defe337e170b
joly001 <joly_huang@163.com>
2019-03-25 14:17:15 +0800

    init

--- src/main/java/com/zcsoft/rc/bms/api/user/entity/RoleAddReq.java
+++ src/main/java/com/zcsoft/rc/bms/api/user/entity/RoleAddReq.java
@@ -1,7 +1,56 @@
 package com.zcsoft.rc.bms.api.user.entity;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
+import java.util.List;
+
 public class RoleAddReq {
 
-    
+    /**
+     * 角色名称
+     */
+    @NotEmpty
+    private String roleName;
+    /**
+     * 角色描述
+     */
+    private String roleDesc;
+    /**
+     * 权限列表
+     */
+    private List<String> authorities;
 
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleDesc() {
+        return roleDesc;
+    }
+
+    public void setRoleDesc(String roleDesc) {
+        this.roleDesc = roleDesc;
+    }
+
+    public List<String> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(List<String> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("RoleAddReq{");
+        sb.append("roleName='").append(roleName).append('\'');
+        sb.append(", roleDesc='").append(roleDesc).append('\'');
+        sb.append(", authorities=").append(authorities);
+        sb.append('}');
+        return sb.toString();
+    }
 }

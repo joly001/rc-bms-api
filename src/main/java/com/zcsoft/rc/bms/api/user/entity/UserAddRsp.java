11b6a73ae8edca51a8fda279cbd23ce2a9ee5dab
joly001 <joly_huang@163.com>
2019-03-26 23:53:44 +0800

    init

---
+++ src/main/java/com/zcsoft/rc/bms/api/user/entity/UserAddRsp.java
@@ -0,0 +1,25 @@
+package com.zcsoft.rc.bms.api.user.entity;
+
+public class UserAddRsp {
+
+    /**
+     * id
+     */
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("UserAddRsp{");
+        sb.append("id='").append(id).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}

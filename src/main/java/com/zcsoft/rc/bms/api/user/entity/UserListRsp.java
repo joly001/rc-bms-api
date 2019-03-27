66672e16bdde77c6119a47cd76a40ede0b4510af
joly001 <joly_huang@163.com>
2019-03-27 13:42:55 +0800

    init

--- src/main/java/com/zcsoft/rc/bms/api/user/entity/UserListRsp.java
+++ src/main/java/com/zcsoft/rc/bms/api/user/entity/UserListRsp.java
@@ -37,4 +37,82 @@ public class UserListRsp {
      */
     private String wristStrapCode;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getBuilderUserType() {
+        return builderUserType;
+    }
+
+    public void setBuilderUserType(String builderUserType) {
+        this.builderUserType = builderUserType;
+    }
+
+    public String getBuilderStatus() {
+        return builderStatus;
+    }
+
+    public void setBuilderStatus(String builderStatus) {
+        this.builderStatus = builderStatus;
+    }
+
+    public Date getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(Date admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+
+    public Date getLeaveDate() {
+        return leaveDate;
+    }
+
+    public void setLeaveDate(Date leaveDate) {
+        this.leaveDate = leaveDate;
+    }
+
+    public String getWristStrapCode() {
+        return wristStrapCode;
+    }
+
+    public void setWristStrapCode(String wristStrapCode) {
+        this.wristStrapCode = wristStrapCode;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("UserListRsp{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", nick='").append(nick).append('\'');
+        sb.append(", mobile='").append(mobile).append('\'');
+        sb.append(", builderUserType='").append(builderUserType).append('\'');
+        sb.append(", builderStatus='").append(builderStatus).append('\'');
+        sb.append(", admissionDate=").append(admissionDate);
+        sb.append(", leaveDate=").append(leaveDate);
+        sb.append(", wristStrapCode='").append(wristStrapCode).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }

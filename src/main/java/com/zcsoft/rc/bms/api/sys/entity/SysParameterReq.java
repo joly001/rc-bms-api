68d068d45ae84196d8e96377910165993f0c1aba
joly001 <joly_huang@163.com>
2019-04-11 15:51:53 +0800

    init

--- src/main/java/com/zcsoft/rc/bms/api/sys/entity/SysParameterReq.java
+++ src/main/java/com/zcsoft/rc/bms/api/sys/entity/SysParameterReq.java
@@ -7,10 +7,6 @@ public class SysParameterReq {
      */
     private String id;
     /**
-     * 参数名称
-     */
-    private String parameterName;
-    /**
      * 参数值
      */
     private String parameterValue;
@@ -21,14 +17,6 @@ public class SysParameterReq {
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getParameterName() {
-        return parameterName;
-    }
-
-    public void setParameterName(String parameterName) {
-        this.parameterName = parameterName;
     }
 
     public String getParameterValue() {
@@ -43,7 +31,6 @@ public class SysParameterReq {
     public String toString() {
         final StringBuilder sb = new StringBuilder("SysParameterReq{");
         sb.append("id='").append(id).append('\'');
-        sb.append(", parameterName='").append(parameterName).append('\'');
         sb.append(", parameterValue='").append(parameterValue).append('\'');
         sb.append('}');
         return sb.toString();
